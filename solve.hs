5d59f8b441a6f0b016c94b0e118edaba1b03bdfe
Matthew Filmer <mattfilmer@gmail.com>
2014-04-27 12:31:58 -0400

    Changed how I calculate derivatives
    Now all derivatives are tracked so the nth derivative can be calculated

--- solve.hs
+++ solve.hs
@@ -21,7 +21,7 @@ module Solve (
   newton, 
   simpNewton, 
   numDeriv,
-  D (D)) where
+  Deriv (D)) where
 
 -- search: Approximate the root of a function given a starting point
 --         and an increment
@@ -87,10 +87,11 @@ numDeriv func x = (func (x + dx) - func (x - dx)) / (2 * dx)
     dx = max 1e-10 (abs x * 1e-10)
 
 -- Automatically differentiate (implementation below)
-autoDeriv :: Num a => (D a -> D a) -> a -> a
-autoDeriv func x = deriv
-  where (D _ deriv) = func (D x 1)
+--autoDeriv :: Num a => (D a -> D a) -> a -> a
+--autoDeriv func x = deriv
+  --where (D _ deriv) = func (D x 1)
 
+{-
 -- Implement Automatic Differentiation
 -- From: http://conal.net/blog/posts/what-is-automatic-differentiation-and-why-does-it-work
 data D a = D a a deriving (Eq,Show)
@@ -125,4 +126,53 @@ instance Floating x => Floating (D x) where
   asinh  (D x x') = D (asinh  x) (x' / sqrt (sqr x + 1))
   atanh  (D x x') = D (atanh  x) (x' / (1 - sqr x))
   acosh  (D x x') = D (acosh  x) (x' / sqrt ((x - 1)*(x + 1)))
+-}
 
+-- Implementation of Automatic Differentiation for an unlimited number of
+-- derivitives
+-- Based on http://conal.net/blog/posts/beautiful-differentiation
+
+-- Datatype
+data Deriv a = D a (Deriv a)
+
+-- Derivative of a constant
+dConst :: Num a => a -> Deriv a
+dConst x0 = D x0 (dConst 0)
+
+-- Implementation of chain rule
+chain f f' u@(D u0 u') = D (f u0) (f' u * u')
+
+-- Squaring function
+sqr x = x*x
+
+-- Basic operators
+instance Num a =>  Num (Deriv a) where
+  D x0 x' + D y0 y'           = D (x0 + y0) (x' + y')
+  D x0 x' - D y0 y'           = D (x0 - y0) (x' - y')
+  x@(D x0 x') * y@(D y0 y')   = D (x0 * y0) (x' * y + x * y')
+  fromInteger                 = dConst . fromInteger
+  negate                      = chain negate (\_ -> dConst (-1))
+  abs                         = chain abs signum
+  signum                      = chain signum (\_ -> dConst (0))
+
+instance Fractional a => Fractional (Deriv a) where
+  x@(D x0 x') / y@(D y0 y')   = D (x0 / y0) ((x' * y - x * y') / (sqr y))
+  fromRational  = dConst . fromRational
+  recip         = chain recip (negate . sqr . recip)
+
+-- Functions
+instance Floating a => Floating (Deriv a) where
+  pi      = dConst pi
+  exp     = chain exp exp
+  log     = chain log recip
+  sqrt    = chain sqrt (recip . (2*) . sqrt)
+  sin     = chain sin cos
+  cos     = chain cos (negate . sin)
+  asin    = chain asin (\x -> recip (sqrt (1 - sqr x)))
+  acos    = chain acos (\x -> - recip (sqrt (1 - sqr x)))
+  atan    = chain atan (\x -> recip (sqr x + 1))
+  sinh    = chain sinh cosh
+  cosh    = chain cosh sinh
+  asinh   = chain asinh (\x -> recip (sqrt (sqr x + 1)))
+  acosh   = chain acosh (\x -> - recip (sqrt (sqr x - 1)))
+  atanh   = chain atanh (\x -> recip (1 - sqr x))
